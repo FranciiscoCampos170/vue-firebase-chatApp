bef898f3f2b60da5cd7f2cc3d6c7e05b148ff7ab
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-26 13:31:18 +0100

    vue-chat-scroll added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueChatScroll from 'vue-chat-scroll'
+
+
+Vue.use(VueChatScroll)
 
 Vue.config.productionTip = false
 
