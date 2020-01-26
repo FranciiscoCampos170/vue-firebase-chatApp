88ff90271d47d91fbe73366e7bfe4bc789fbb0ae
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-26 04:10:36 +0100

    firebase config

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCBEELtAOm7YYBwd1gcxzL4Qonde1Arp4U",
+  authDomain: "vue-chatapp-d4244.firebaseapp.com",
+  databaseURL: "https://vue-chatapp-d4244.firebaseio.com",
+  projectId: "vue-chatapp-d4244",
+  storageBucket: "vue-chatapp-d4244.appspot.com",
+  messagingSenderId: "81093705035",
+  appId: "1:81093705035:web:418128c7d1749e9abc3cce",
+  measurementId: "G-2H2BYFHQ2M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
