3c9a10ffadec9655ffb0393911ee201f3c0644b0
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-26 11:44:33 +0100

    newMessege componten added

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,16 +12,20 @@
                 </ul>
             </div>
             <div class="card-action">
-                <input type="text">
+                <NewMessage :name="name"/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
 export default {
     name:'Chat',
     props: ['name'],
+    components: {
+        NewMessage
+    }, 
     data(){
         return {
 
